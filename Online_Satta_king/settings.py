2c71f57750455a4c5c0868468b842d19ff68faa4
rohitd46 <rdevra46@gmail.com>
2022-10-10 18:25:18 +0530

    10/10

--- Online_Satta_king/settings.py
+++ Online_Satta_king/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'Online_Satta_king.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sattakingdb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'root',
+        'HOST' :   'localhost',
+        'POST' :   5432
+            }
 }
 
 
